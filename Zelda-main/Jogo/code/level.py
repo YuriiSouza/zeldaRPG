13a081a7dd2bdbe078d13bf3217f09952a62a876
Yuri <86446634+YuriiSouza@users.noreply.github.com>
2023-06-27 23:09:26 -0300

    .

--- Zelda-main/Jogo/code/level.py
+++ Zelda-main/Jogo/code/level.py
@@ -17,6 +17,7 @@ class Level:
         #sprite setup
         self.create_map()
         
+        
     def create_map(self):
         for row_index, row in enumerate(WORLD_MAP):
             for col_index, col in enumerate(row):
@@ -44,6 +45,10 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
+        
+        #creating the floor
+        self.floor_surf = pygame.image.load('/graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0, 0))
     
     def custom_draw(self, player):
         
@@ -51,7 +56,6 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
         
-        #for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset  
             self.display_surface.blit(sprite.image,offset_pos)
